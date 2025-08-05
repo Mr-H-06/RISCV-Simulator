19a5ba3584a71ed7eca8422d8a0d2a0abdc2c73e
Mr-H-06 <he-jiajun@sjtu.edu.cn>
2025-08-05 11:54:59 +0800

    expr

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "simulator.h"
 
 int main() {
-  std::string filename = "../testcases/bulgarian.data";
-  Simulator simulator(filename);
-  //Simulator simulator = Simulator();
+  //std::string filename = "../testcases/bulgarian.data";
+  //Simulator simulator(filename);
+  Simulator simulator = Simulator();
   simulator.run();
   return 0;
 }
--- src/rob.h
+++ src/rob.h
@@ -164,7 +164,7 @@ public:
         next.rob[i].prepared = false;
       }
     }
-    std::cerr << next.rob[next.head].instruction.pc << '\n';
+    //std::cerr << next.rob[next.head].instruction.pc << '\n';
     /*for (int i = 0; i < 32; ++i) {
       std::cerr << "[x" << i << " = " << next.rf.reg[i] << "]\n";
     }*/
